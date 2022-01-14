271753a419646ffa6cf5137d7e389af3cdd7ce0d
Guilherme Leonardo Nunes <guilherme.l.nunes@gmail.com>
2022-01-14 21:13:06 +0000

    changed parameter name

--- app/cmd/fetch.go
+++ app/cmd/fetch.go
@@ -2,31 +2,35 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
 
 var (
-	templatePath string
-	folderName   string
+	repoTemplatePath string
+	docTemplatePath  string
+	folderName       string
 )
 
 var fetchCmd = &cobra.Command{
 	Use:   "fetch",
 	Short: "fetches template",
 	Run: func(cmd *cobra.Command, args []string) {
-		if templatePath == "" {
-			panic(fmt.Errorf("path argument needs to be specified"))
+		if (repoTemplatePath == "" && docTemplatePath == "") || (repoTemplatePath != "" && docTemplatePath != "") {
+			fmt.Println("exactly only one flag from repo or doc should be specified")
+			os.Exit(1)
 		}
 
-		if err := fetcherApplication.Fetch(templatePath, folderName); err != nil {
+		if err := fetcherApplication.Fetch(repoTemplatePath, folderName); err != nil {
 			panic(err)
 		}
 	},
 }
 
 func init() {
-	fetchCmd.PersistentFlags().StringVar(&templatePath, "path", "", "path=[path/to/your/repo]")
+	fetchCmd.PersistentFlags().StringVar(&repoTemplatePath, "repo", "", "template=[path/to/your/template_repo]")
+	fetchCmd.PersistentFlags().StringVar(&docTemplatePath, "doc", "", "doc=[path/to/your/template_doc]")
 	fetchCmd.PersistentFlags().StringVar(&folderName, "name", "", "name=[folder to dump template]")
 	rootCmd.AddCommand(fetchCmd)
 }
